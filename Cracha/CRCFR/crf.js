586be912e04419a3e780ffefe1a65b9006c82c4e
magafafinhos <145406233+magafafinhos@users.noreply.github.com>
2023-10-06 11:59:02 -0300

    Crachas

---
+++ Cracha/CRCFR/crf.js
@@ -0,0 +1,27 @@
+function generateBadge() {
+    const imageInput = document.getElementById('image');
+    const nameInput = document.getElementById('name').value;
+    const roleInput = document.getElementById('role').value;
+
+    const badgeImage = document.getElementById('badgeImage');
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      badgeImage.src = e.target.result;
+    }
+    reader.readAsDataURL(imageInput.files[0]);
+
+    document.getElementById('badgeName').textContent = nameInput;
+    document.getElementById('badgeRole').textContent = roleInput;
+
+    document.getElementById('badgeContainer').style.display = 'flex';
+  }
+
+function printContent(el){
+
+    var restorepage = document.body.innerHTML;
+    var printContent = document.getElementById(el).innerHTML;
+    document.body.innerHTML = printContent;
+    window.print();
+    document.body.innerHTML = restorepage
+
+}
